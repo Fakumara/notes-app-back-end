daf995ba29095119d4d619c93b062bea4565b624
Faiz Kumara <faizkumara@gmail.com>
2024-08-05 12:55:57 +0700

    fix bugs note value

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,15 @@ const notes = require('./notes');
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
 
+  if (!title || !tags || !body) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Data catatan tidak lengkap',
+    });
+    response.code(400);
+    return response;
+  }
+
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
@@ -15,19 +24,20 @@ const addNoteHandler = (request, h) => {
 
   notes.push(newNote);
 
-  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+  const isSuccess = notes.some((note) => note.id === id);
 
   if (isSuccess) {
     const response = h.response({
       status: 'success',
       message: 'Catatan berhasil ditambahkan',
       data: {
-        noteId: id,
+        note: newNote,
       },
     });
     response.code(201);
     return response;
   }
+
   const response = h.response({
     status: 'fail',
     message: 'Catatan gagal ditambahkan',
